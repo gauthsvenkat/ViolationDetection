f7ee495b4139db2a97e12ecf3afbc3e1273cd83f
andohuman <gauthsvenkat@gmail.com>
2019-08-08 13:26:03 +0530

    fixed a bug in visualization and changed train arguments to default

--- keras_retinanet/utils/visualization.py
+++ keras_retinanet/utils/visualization.py
@@ -33,7 +33,7 @@ def draw_box(image, box, color, thickness=2):
     cv2.rectangle(image, (b[0], b[1]), (b[2], b[3]), color, thickness, cv2.LINE_AA)
 
 
-def draw_caption(image, box, caption, color):
+def draw_caption(image, box, caption, color=(255,255,255)):
     """ Draws a caption above the box in an image.
 
     # Arguments
